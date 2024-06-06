2907cf08afabe7b97a431109d0ed713d8ced8c46
laboratorio <coyojavierelizabeth@gmail.com>
2024-06-06 16:10:59 -0400

    se agrega el proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MenuComponent } from './sitio/menu/menu.component';
+import { SliderComponent } from './sitio/slider/slider.component';
+import { BodyComponent } from './sitio/body/body.component';
+import { FooterComponent } from './sitio/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,MenuComponent,
+    SliderComponent,BodyComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
